131d37d788bcd66e47f9c7d9fe27fee242f8705f
Johnny9Rodriguez <mail@joepytlik.de>
2024-09-16 23:08:16 +0200

    feat: move boot logic into file

--- main.js
+++ main.js
@@ -1,40 +1,10 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
-import { loadBridgeData } from './src/bridge-utils/bridgeData.js';
-import { setupTray } from './src/app-utils/setupTray.js';
-import { setWindowControls } from './src/ipc-handlers/setWindowControls.js';
-import { setStaticDataHandlers } from './src/ipc-handlers/setStaticDataHandlers.js';
+import { app } from 'electron';
+import { boot } from './src/app-utils/bootApp.js';
 
 const ROOT_DIR = import.meta.dirname;
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        // width: 725,
-        width: 1000,
-        height: 500,
-        minWidth: 725,
-        minHeight: 500,
-        frame: false,
-        webPreferences: {
-            preload: path.join(ROOT_DIR, 'preload.js'),
-        },
-    });
-
-    win.loadURL('http://localhost:3000');
-    win.webContents.openDevTools();
-    win.setMenuBarVisibility(false);
-
-    return win;
-};
-
 app.whenReady().then(async () => {
-    loadBridgeData(ROOT_DIR);
-
-    const win = createWindow();
-    setupTray(win);
-
-    setWindowControls(app, win);
-    setStaticDataHandlers();
+    boot(ROOT_DIR);
 });
 
 app.on('window-all-closed', () => {
