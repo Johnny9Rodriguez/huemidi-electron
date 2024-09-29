a7d6c4f5e7feb1e469efcd452be3ce3bf2e900ad
Johnny9Rodriguez <mail@joepytlik.de>
2024-09-29 14:57:49 +0200

    fix: disable dev console

--- src/app-utils/windowManager.js
+++ src/app-utils/windowManager.js
@@ -62,15 +62,15 @@ const closeSetupWindow = () => {
 
 const createMainWindow = () => {
     mainWindow = new BrowserWindow({
-        // width: 725,
-        width: 1000,
+        width: 725,
+        // width: 1000,
         height: 500,
         minWidth: 725,
         minHeight: 500,
         frame: false,
         webPreferences: {
             preload: path.join(ROOT_DIR, 'preload.js'),
-            // devTools: false,
+            devTools: false,
         },
         icon: path.join(ROOT_DIR, 'public/images', 'logo-32.ico'),
     });
@@ -79,7 +79,7 @@ const createMainWindow = () => {
     mainWindow.loadURL(
         `file://${path.join(ROOT_DIR, 'build', 'index.html')}#/`
     );
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
     mainWindow.setMenuBarVisibility(false);
 
     // Disable reload with Ctrl + R
